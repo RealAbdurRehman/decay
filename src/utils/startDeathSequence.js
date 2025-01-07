717d112418d0419cabe2b5cbba9c6c1df3e3095b
Abdur Rehman <claudethebitplayer@gmail.com>
2025-01-07 10:41:46 +0500

    Update startDeathSequence.js

--- src/utils/startDeathSequence.js
+++ src/utils/startDeathSequence.js
@@ -1,7 +1,8 @@
 const deathScreen = document.getElementById("death-screen");
 const killDisplay = document.getElementById("kills-stat");
 
-export default function startDeathSequence(enemiesKilled) {
+export default function startDeathSequence(enemiesKilled, controls) {
   deathScreen.style.display = "flex";
   killDisplay.innerHTML = enemiesKilled;
+  controls.unlock();
 }
